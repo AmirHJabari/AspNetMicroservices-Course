6937d80ae0e28dcecad9366984dc7393538ec4aa
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-12-01 19:31:47 +0330

    Implement Ordering.API

---
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.DeleteOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get([Required] string username, CancellationToken cancellationToken)
+        {
+            GetOrdersQuery query = new(username);
+            var order = await _mediator.Send(query, cancellationToken);
+
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CheckoutOrderCommand command, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+
+            return Ok(new { Id = result });
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult> Put([FromBody] UpdateOrderCommand command, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(command, cancellationToken);
+            
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult> Delete(int id, CancellationToken cancellationToken)
+        {
+            DeleteOrderCommand command = new(id);
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}
