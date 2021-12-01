80c56097c3ce3aa59aed5afe94093dee49e29b03
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-12-01 19:30:40 +0330

    Implement Ordering.Domain

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase<TKey> : IEntity
+    {
+        public virtual TKey Id { get; set; }
+
+        public virtual DateTimeOffset CreatedDate { get; set; }
+        public virtual DateTimeOffset LastModifiedDate { get; set; }
+    }
+
+    public abstract class EntityBase : EntityBase<int>
+    {
+    }
+}
