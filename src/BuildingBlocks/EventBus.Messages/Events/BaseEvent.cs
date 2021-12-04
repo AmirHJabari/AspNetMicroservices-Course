a1735c463036f9418dc140f10614622b42ac5785
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-12-04 15:23:53 +0330

    Implement EventBus.Messages common library.

---
+++ src/BuildingBlocks/EventBus.Messages/Events/BaseEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EventBus.Messages.Events
+{
+    public class BaseEvent
+    {
+        public BaseEvent()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreationDate = DateTimeOffset.UtcNow;
+        }
+
+        public BaseEvent(Guid id, DateTimeOffset creationDate)
+        {
+            this.Id = id;
+            this.CreationDate = creationDate;
+        }
+
+        public Guid Id { get; }
+        public DateTimeOffset CreationDate { get; }
+    }
+}
